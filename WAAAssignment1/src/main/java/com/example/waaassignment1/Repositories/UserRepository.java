e6d83c1bb77048310b68099d88ef329cfdc1c714
diaanassar <43388125+diaanassar@users.noreply.github.com>
2023-07-24 18:13:50 -0500

    Update UserRepository.java

--- WAAAssignment1/src/main/java/com/example/waaassignment1/Repositories/UserRepository.java
+++ WAAAssignment1/src/main/java/com/example/waaassignment1/Repositories/UserRepository.java
@@ -4,7 +4,6 @@ import com.example.waaassignment1.Entities.User;
 
 public class UserRepository {
     public void saveUser(User user) {
-        // Implementation to save user to a database or some other storage
         System.out.println("User saved: " + user.getName());
     }
 }
