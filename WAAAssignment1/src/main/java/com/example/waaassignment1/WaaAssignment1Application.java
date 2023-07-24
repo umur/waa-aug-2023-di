9a85fc71e06282845b4bc9aeaf46701514dfc3e5
diaanassar <43388125+diaanassar@users.noreply.github.com>
2023-07-24 18:15:53 -0500

    Update WaaAssignment1Application.java

--- WAAAssignment1/src/main/java/com/example/waaassignment1/WaaAssignment1Application.java
+++ WAAAssignment1/src/main/java/com/example/waaassignment1/WaaAssignment1Application.java
@@ -13,19 +13,17 @@ public class WaaAssignment1Application {
     public static void main(String[] args) {
         SpringApplication.run(WaaAssignment1Application.class, args);
 
-        //Creating an instance of the Container
         Container container = new Container();
 
-        // Registering UserRepository as a dependency in the Container
         UserRepository userRepository = new UserRepository();
         container.registerDependency(UserRepository.class, userRepository);
 
-        // Creating an instance of MyDependencyInjector and inject dependencies into UserService
+        // injecting the dependencies into UserService::
         UserService userService = new UserService();
         MyDependencyInjector injector = new MyDependencyInjector(container);
         injector.injectDependencies(userService);
 
-        // Verifying the injection by using the UserService
+        // Verifying the injection by using the UserService:
         userService.performAction();
 
     }
