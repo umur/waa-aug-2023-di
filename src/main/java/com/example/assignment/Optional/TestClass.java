e2af54a7f050d1057ada805e1fb970f470d198a9
dainomyohannes <yohannesdainom@gmail.com>
2023-07-27 00:50:52 -0500

    lab 1

---
+++ src/main/java/com/example/assignment/Optional/TestClass.java
@@ -0,0 +1,13 @@
+package com.example.assignment.Optional;
+
+import com.example.assignment.Myframework.TsiDai;
+
+public class TestClass {
+
+    @TsiDai
+    private Dependecy1 dependecy1;
+
+    public Dependecy1 getDependecy1() {
+        return dependecy1;
+    }
+}
